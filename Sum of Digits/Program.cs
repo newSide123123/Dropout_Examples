7ee45552d33a900b315f97c525b55a403b1aa072
newSide123123 <newSide01901@tutamail.com>
2024-07-25 14:16:48 +0300

    Implement dark mode

---
+++ Sum of Digits/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Sum_of_Digits
+{
+    internal class Program
+    {
+        public static int getNumberFromString(string s)
+        {
+            return Convert.ToInt32(new string(s.Where(char.IsDigit).ToArray()));
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(getNumberFromString("hell5o wor6ld"));
+        }
+    }
+}
